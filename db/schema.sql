23448da854ea0f8bf71a55fad5085fa2e670da5f
Roger <rdpender3@gmail.com>
2020-01-15 18:23:35 -0800

    changed db

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,13 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
+CREATE DATABASE test_2_burgers_db;
+USE test_2_burgers_db;
 
-CREATE TABLE burgers(
-    id INT AUTO_INCREMENT NOT NULL,
-    burger_name VARCHAR(255) NOT NULL,
+CREATE TABLE burgers
+(
+    id INT
+    AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR
+    (255) NOT NULL,
     devoured BOOLEAN NOT NULL DEFAULT 0,
-    PRIMARY KEY (id)
+    PRIMARY KEY
+    (id)
 );
